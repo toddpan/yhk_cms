8b51c18b8fb21e38e15643a449d5af8b9a1c7748
ant_shake_tree <ant_shake_tree@163.com>
2015-08-28 14:38:08 +0800

    config rename

---
+++ src/main/test/com/ykh/dao/conference/ConferenceSeedDaoTest.java
@@ -0,0 +1,16 @@
+package com.ykh.dao.conference;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ant_shake_tree on 15/8/28.
+ */
+public class ConferenceSeedDaoTest {
+
+    @Test
+    public void testFindMaxId() throws Exception {
+
+    }
+}
