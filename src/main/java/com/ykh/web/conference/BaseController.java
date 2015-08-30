e8957ecc1bd88aa2059972b074c319a91d3dfd3d
ant_shake_tree <ant_shake_tree@163.com>
2015-08-30 21:42:49 +0800

    修改创建会议

--- src/main/java/com/ykh/web/conference/BaseController.java
+++ src/main/java/com/ykh/web/conference/BaseController.java
@@ -2,6 +2,8 @@ package com.ykh.web.conference;
 
 import com.maxc.rest.common.exception.ExceptionCode;
 import com.maxc.rest.common.exception.RestException;
+import com.ykh.conference.service.exception.CMSErrorCode;
+import com.ykh.tang.agent.excep.CMSException;
 import com.ykh.vo.res.Response;
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,7 +27,12 @@ public class BaseController {
 			re.getHead().setError(resstException.getErrorCode());
 			re.getHead().setMessage(ex.getMessage());
 			return re;
-		}else{
+		}else if (ex instanceof CMSException){
+
+			re.getHead().setError(Integer.parseInt(((CMSException) ex).getCodeID()));
+			re.getHead().setMessage("CMS exception.");
+			return re;
+		} else{
 			re.getHead().setError(ExceptionCode.UnknowException.getErrorCode());
 			re.getHead().setMessage("unknowException ["+ex.getMessage()+"]");
 			return re;
