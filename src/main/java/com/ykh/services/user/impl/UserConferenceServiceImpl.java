5577e3acd5c4a620127dde016ad9ae3c2be4ecab
ant_shake_tree <ant_shake_tree@163.com>
2015-09-03 00:14:02 +0800

    rebuild

--- src/main/java/com/ykh/services/user/impl/UserConferenceServiceImpl.java
+++ src/main/java/com/ykh/services/user/impl/UserConferenceServiceImpl.java
@@ -82,12 +82,12 @@ public class UserConferenceServiceImpl implements UserConferenceService {
         tempUser.setUserChannel(userChannel);
         //修改
         tempUserDao.save(tempUser);
-        logger.info("userJoinConference  ===> jni end UserChannel" + JSON.toJSONString(userChannel));
+        logger.info("userJoinConference  ===> jni end UserChannel" + ParseJSON.toJson(userChannel));
         logger.info("userJoinConference  ===> jni end UserChannel"+ userChannel.getCtsAddr().get(0).getHotServerIP0());
         logger.info("userJoinConference  ===> jni end UserChannel"+ userChannel.getCtsAddr().get(0).getHotServerIP1());
         logger.info("userJoinConference  ===> jni end UserChannel"+ userChannel.getCtsAddr().get(0).getHotServerIP2());
         logger.info("userJoinConference  ===> jni end UserChannel"+ userChannel.getCtsAddr().get(0).getHotServerIP3());
-        
+
 
         userChannel.setTempConferenceID(request.getTempConferenceId());
         return userChannel;
