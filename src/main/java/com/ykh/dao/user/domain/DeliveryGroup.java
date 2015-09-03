d2474d906c76016d06ffd5cde341b2783d74bf62
ant_shake_tree <ant_shake_tree@163.com>
2015-09-04 07:21:57 +0800

    最新修改

--- src/main/java/com/ykh/dao/user/domain/DeliveryGroup.java
+++ src/main/java/com/ykh/dao/user/domain/DeliveryGroup.java
@@ -1,84 +1,84 @@
-/**
- * Project Name:vdi-core-server-lls
- * File Name:Delivery.java
- * Package Name:com.vdi.dao.user.domain
- * Date:2014年8月14日下午3:30:19
- * Copyright (c) 2014 All Rights Reserved.
- *
-*/
-
-package com.ykh.dao.user.domain;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import com.ykh.common.cache.CacheDomain;
-import com.ykh.dao.Request;
-/**
- *
- * ClassName: Group <br/>
- * Function: Delivery group, is used to allocate resources (Desktoppool). Delivery group is many-to-many relationship with the user. <br/>
- * date: 2014年8月15日 上午8:59:37 <br/>
- *
- * @author maxiaochao
- * @version
- * @since JDK 1.7
- */
-@Entity
-@Table(name="deliverygroup")
-public class DeliveryGroup implements Request<DeliveryGroup>,CacheDomain{
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer idgroup;
-	private String name;
-	@ManyToMany(cascade = CascadeType.REFRESH,fetch=FetchType.EAGER)
-	@JoinTable(name = "groupsusers", inverseJoinColumns = @JoinColumn(name = "groupid"), joinColumns = @JoinColumn(name = "userid"))
-	private Set<User> users;
-
-	public Integer getIdgroup() {
-		return idgroup;
-	}
-	public void setIdgroup(Integer idgroup) {
-		this.idgroup = idgroup;
-	}
-	public String getName() {
-		return name;
-	}
-	public DeliveryGroup setName(String name) {
-		this.name = name;
-		return this;
-	}
-	public Set<User> getUsers() {
-		return users;
-	}
-	public void setUsers(Set<User> users) {
-		this.users = users;
-
-	}
-	public DeliveryGroup addUser(User user){
-		if(this.getUsers()==null){
-			this.setUsers(new HashSet<User>());
-		}
-		this.getUsers().add(user);
-		return this;
-	}
-	@Override
-	@Transient
-	public Object getId() {
-		return this.getIdgroup();
-	}
-
-
-}
+///**
+// * Project Name:vdi-core-server-lls
+// * File Name:Delivery.java
+// * Package Name:com.vdi.dao.user.domain
+// * Date:2014年8月14日下午3:30:19
+// * Copyright (c) 2014 All Rights Reserved.
+// *
+//*/
+//
+//package com.ykh.dao.user.domain;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Entity;
+//import javax.persistence.FetchType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.JoinTable;
+//import javax.persistence.ManyToMany;
+//import javax.persistence.Table;
+//import javax.persistence.Transient;
+//
+//import com.ykh.common.cache.CacheDomain;
+//import com.ykh.dao.Request;
+///**
+// *
+// * ClassName: Group <br/>
+// * Function: Delivery group, is used to allocate resources (Desktoppool). Delivery group is many-to-many relationship with the user. <br/>
+// * date: 2014年8月15日 上午8:59:37 <br/>
+// *
+// * @author maxiaochao
+// * @version
+// * @since JDK 1.7
+// */
+//@Entity
+//@Table(name="deliverygroup")
+//public class DeliveryGroup implements Request<DeliveryGroup>,CacheDomain{
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.AUTO)
+//	private Integer idgroup;
+//	private String name;
+//	@ManyToMany(cascade = CascadeType.REFRESH,fetch=FetchType.EAGER)
+//	@JoinTable(name = "groupsusers", inverseJoinColumns = @JoinColumn(name = "groupid"), joinColumns = @JoinColumn(name = "userid"))
+//	private Set<User> users;
+//
+//	public Integer getIdgroup() {
+//		return idgroup;
+//	}
+//	public void setIdgroup(Integer idgroup) {
+//		this.idgroup = idgroup;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public DeliveryGroup setName(String name) {
+//		this.name = name;
+//		return this;
+//	}
+//	public Set<User> getUsers() {
+//		return users;
+//	}
+//	public void setUsers(Set<User> users) {
+//		this.users = users;
+//
+//	}
+//	public DeliveryGroup addUser(User user){
+//		if(this.getUsers()==null){
+//			this.setUsers(new HashSet<User>());
+//		}
+//		this.getUsers().add(user);
+//		return this;
+//	}
+//	@Override
+//	@Transient
+//	public Object getId() {
+//		return this.getIdgroup();
+//	}
+//
+//
+//}
--- src/main/java/com/ykh/dao/user/domain/User.java
+++ src/main/java/com/ykh/dao/user/domain/User.java
@@ -1,217 +1,217 @@
-/**
- * Title: TicketWrapper.java
- * @Package com.crawler.pojo
- * : TicketWrapper.java
- * @author david
- * @date 2013-2-2 上午12:33:22
- * @version
- */
-package com.ykh.dao.user.domain;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Transient;
-
-import javax.persistence.Table;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import com.ykh.common.cache.CacheDomain;
-import com.ykh.dao.Request;
-
-/**
- * ClassName: TicketWrapper :
- *
- * @author david
- * @date 2013-2-2 上午12:33:22
- */
-@Entity
-@Table
-public class User implements UserDetails,CacheDomain,Request<User> {
-	/**
-	 * @Fields serialVersionUID
-	 */
-	private static final long serialVersionUID = -3055321652817602345L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer iduser;
-	private String username;
-	private String password;
-	private boolean enabled = true;
-	private String realname;
-	@ManyToMany(cascade=CascadeType.REFRESH,targetEntity=DeliveryGroup.class,mappedBy="users")
-	private Set<DeliveryGroup> groups=new HashSet<DeliveryGroup>();
-	private String mobile;
-	private String email;
-	@OneToOne(optional=true,fetch=FetchType.LAZY,targetEntity=Domain.class,cascade=CascadeType.ALL)
-	@JoinColumn(name="domainguid")
-	private Domain domain;
-	@ManyToMany(cascade = { CascadeType.ALL }, targetEntity = Role.class, fetch = FetchType.EAGER)
-	@JoinTable(name = "userroles", inverseJoinColumns = @JoinColumn(name = "userid"), joinColumns = @JoinColumn(name = "roleid"))
-	private Set<Role> roles = new HashSet<Role>();
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "organizationid")////这里设置JoinColum设置了外键的名字，并且organization是关系维护端
-	private Organization organization;
-	private String telephone;
-	private String address;
-	private String notes;
-
-
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getNotes() {
-		return notes;
-	}
-
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public Integer getIduser() {
-		return iduser;
-	}
-
-	public void setIduser(Integer iduser) {
-		this.iduser = iduser;
-	}
-
-	public String getRealname() {
-		return realname;
-	}
-
-	public void setRealname(String realname) {
-		this.realname = realname;
-	}
-
-
-
-	public Set<DeliveryGroup> getGroups() {
-		return groups;
-	}
-
-	public void setGroups(Set<DeliveryGroup> groups) {
-		this.groups = groups;
-	}
-
-	public String getMobile() {
-		return mobile;
-	}
-
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return false;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return false;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return false;
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.getRoles();
-	}
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-
-	public Domain getDomain() {
-		return domain;
-	}
-
-	public void setDomain(Domain domain) {
-		this.domain = domain;
-	}
-
-	public Organization getOrganization() {
-		return organization;
-	}
-
-	public void setOrganization(Organization organization) {
-		this.organization = organization;
-	}
-
-	@Override
-	@Transient
-	public Object getId() {
-		return this.getIduser();
-	}
-
-
-
-}
+///**
+// * Title: TicketWrapper.java
+// * @Package com.crawler.pojo
+// * : TicketWrapper.java
+// * @author david
+// * @date 2013-2-2 上午12:33:22
+// * @version
+// */
+//package com.ykh.dao.user.domain;
+//
+//import java.util.Collection;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Set;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Entity;
+//import javax.persistence.FetchType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.JoinTable;
+//import javax.persistence.ManyToMany;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.OneToOne;
+//import javax.persistence.Transient;
+//
+//import javax.persistence.Table;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import com.ykh.common.cache.CacheDomain;
+//import com.ykh.dao.Request;
+//
+///**
+// * ClassName: TicketWrapper :
+// *
+// * @author david
+// * @date 2013-2-2 上午12:33:22
+// */
+//@Entity
+//@Table
+//public class User implements UserDetails,CacheDomain,Request<User> {
+//	/**
+//	 * @Fields serialVersionUID
+//	 */
+//	private static final long serialVersionUID = -3055321652817602345L;
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Integer iduser;
+//	private String username;
+//	private String password;
+//	private boolean enabled = true;
+//	private String realname;
+//	@ManyToMany(cascade=CascadeType.REFRESH,targetEntity=DeliveryGroup.class,mappedBy="users")
+//	private Set<DeliveryGroup> groups=new HashSet<DeliveryGroup>();
+//	private String mobile;
+//	private String email;
+//	@OneToOne(optional=true,fetch=FetchType.LAZY,targetEntity=Domain.class,cascade=CascadeType.ALL)
+//	@JoinColumn(name="domainguid")
+//	private Domain domain;
+//	@ManyToMany(cascade = { CascadeType.ALL }, targetEntity = Role.class, fetch = FetchType.EAGER)
+//	@JoinTable(name = "userroles", inverseJoinColumns = @JoinColumn(name = "userid"), joinColumns = @JoinColumn(name = "roleid"))
+//	private Set<Role> roles = new HashSet<Role>();
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "organizationid")////这里设置JoinColum设置了外键的名字，并且organization是关系维护端
+//	private Organization organization;
+//	private String telephone;
+//	private String address;
+//	private String notes;
+//
+//
+//
+//	public String getTelephone() {
+//		return telephone;
+//	}
+//
+//	public void setTelephone(String telephone) {
+//		this.telephone = telephone;
+//	}
+//	public String getAddress() {
+//		return address;
+//	}
+//
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}
+//
+//	public String getNotes() {
+//		return notes;
+//	}
+//
+//	public void setNotes(String notes) {
+//		this.notes = notes;
+//	}
+//
+//	public String getUsername() {
+//		return username;
+//	}
+//
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	public boolean isEnabled() {
+//		return enabled;
+//	}
+//
+//	public void setEnabled(boolean enabled) {
+//		this.enabled = enabled;
+//	}
+//
+//	public Integer getIduser() {
+//		return iduser;
+//	}
+//
+//	public void setIduser(Integer iduser) {
+//		this.iduser = iduser;
+//	}
+//
+//	public String getRealname() {
+//		return realname;
+//	}
+//
+//	public void setRealname(String realname) {
+//		this.realname = realname;
+//	}
+//
+//
+//
+//	public Set<DeliveryGroup> getGroups() {
+//		return groups;
+//	}
+//
+//	public void setGroups(Set<DeliveryGroup> groups) {
+//		this.groups = groups;
+//	}
+//
+//	public String getMobile() {
+//		return mobile;
+//	}
+//
+//	public void setMobile(String mobile) {
+//		this.mobile = mobile;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	@Override
+//	public boolean isAccountNonExpired() {
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean isAccountNonLocked() {
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean isCredentialsNonExpired() {
+//		return false;
+//	}
+//
+//	@Override
+//	public Collection<? extends GrantedAuthority> getAuthorities() {
+//		return this.getRoles();
+//	}
+//
+//	public Set<Role> getRoles() {
+//		return roles;
+//	}
+//
+//	public void setRoles(Set<Role> roles) {
+//		this.roles = roles;
+//	}
+//
+//	public Domain getDomain() {
+//		return domain;
+//	}
+//
+//	public void setDomain(Domain domain) {
+//		this.domain = domain;
+//	}
+//
+//	public Organization getOrganization() {
+//		return organization;
+//	}
+//
+//	public void setOrganization(Organization organization) {
+//		this.organization = organization;
+//	}
+//
+//	@Override
+//	@Transient
+//	public Object getId() {
+//		return this.getIduser();
+//	}
+//
+//
+//
+//}
